70cfa5ddfdd933827d3fd617f3036c50c36a2ae8
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-03 17:01:47 +0530

    responsive menu

--- src/App.js
+++ src/App.js
@@ -6,8 +6,24 @@ function App() {
   let [status, setStatus] = useState(false)
 
   let [pstatus, setPStatus] = useState(false)
+
+  let [menuStatus, setMenuStatus] = useState(false)
   return (
     <div className="App">
+      <button className='micon' onClick={() => setMenuStatus(!menuStatus)}>
+        {menuStatus ? <span>&times;</span> : <span>&#9776;</span>}
+
+      </button>
+      <div className={`menu ${menuStatus ? 'activeMenu' : ''}`}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Courses</li>
+          <li>Gallery</li>
+          <li>About Us</li>
+        </ul>
+      </div>
+      <br /><br />
       <div>
         <input type={pstatus ? 'text' : 'password'} /> <button onClick={() => setPStatus(!pstatus)}>{pstatus ? 'Hide' : 'Show'}</button>
       </div>
