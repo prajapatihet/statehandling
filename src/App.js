98e6233dbdda86715d6a22d9cdf322a918ccf095
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-03 11:45:03 +0530

    ternary operator of if-else

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [status, setStatus] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setStatus(!status)}>Show</button>
+      {status ? <p className='para'>Welcome to Page</p> : ''}
+
     </div>
   );
 }
