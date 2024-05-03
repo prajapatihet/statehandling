b8dc9655e1a70ec9e1d22b9759cd558a5b372081
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-03 16:31:19 +0530

    password hide&show

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import btnModule from './Button.module.css'
 
 function App() {
   let [status, setStatus] = useState(false)
+
+  let [pstatus, setPStatus] = useState(false)
   return (
     <div className="App">
-      <button className={btnModule.error}>Error</button>
-      <button className={btnModule.warning}>Warning</button>
-      <button onClick={() => setStatus(!status)}>{(status ? 'Hide' : 'Show')}</button>
-      {status ? <p className='para'>Welcome to Page</p> : ''}
+      <div>
+        <input type={pstatus ? 'text' : 'password'} /> <button onClick={() => setPStatus(!pstatus)}>{pstatus ? 'Hide' : 'Show'}</button>
+      </div>
+      <br />
+      <div>
+        <button className={btnModule.error}>Error</button>
+        <button className={btnModule.warning}>Warning</button>
+        <button onClick={() => setStatus(!status)}>{(status ? 'Hide' : 'Show')}</button>
+        {status ? <p className='para'>Welcome to Page</p> : ''}
+      </div>
+
 
     </div>
   );
