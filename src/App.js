e2699a15abe14bfd5c8d4bab7875be6394575c85
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-03 16:14:26 +0530

    module style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import btnModule from './Button.module.css'
 
 function App() {
   let [status, setStatus] = useState(false)
   return (
     <div className="App">
+      <button className={btnModule.error}>Error</button>
+      <button className={btnModule.warning}>Warning</button>
       <button onClick={() => setStatus(!status)}>{(status ? 'Hide' : 'Show')}</button>
       {status ? <p className='para'>Welcome to Page</p> : ''}
 
