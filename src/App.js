06f9c7a6457bba6c8b678927bf8c398a84982f9b
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-17 19:38:07 +0530

    react notification

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // import { useState } from 'react';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 import './App.css';
 // import Faqs from './Faqs'
 // import btnModule from './Button.module.css';
 // import { questions } from './Data/faqs';
+import 'react-notifications/lib/notifications.css';
 
 function App() {
   // let [status, setStatus] = useState(false)
@@ -15,15 +17,56 @@ function App() {
 
   // let [showAns, setShowAns] = useState(questions[0].id)
 
-  let n = ["ram", "rav", "raj", "hello"];
+  // let n = ["ram", "rav", "raj", "hello"];
+
+  let createNotification = (t) => {
+    return () => {
+      // eslint-disable-next-line
+      switch (t) {
+        case 'info':
+          NotificationManager.info('Info message');
+          break;
+        case 'success':
+          NotificationManager.success('Success message', 'Title here');
+          break;
+        case 'warning':
+          NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
+          break;
+        case 'error':
+          NotificationManager.error('Error message', 'Click me!', 5000, () => {
+            alert('callback');
+          });
+          break;
+      }
+    };
+  };
+
   return (
     <div className="App">
+      <NotificationContainer />
+      <h2>React Notification</h2>
+      <button className='btn btn-info'
+        onClick={createNotification('info')}>Info
+      </button>
+      <hr />
+      <button
+        onClick={createNotification('success')}>Success
+      </button>
+      <hr />
+      <button
+        onClick={createNotification('warning')}>Warning
+      </button>
+      <hr />
+      <button
+        onClick={createNotification('error')}>Error
+      </button>
 
-      {n.map((item, i) => {
+
+      {/* {n.map((item, i) => {
         return (
           <li key={i}>{item}</li>
         )
-      })}
+      })} */}
 
 
 
@@ -74,7 +117,7 @@ function App() {
         <input type={pstatus ? 'text' : 'password'} /> <button onClick={() => setPStatus(!pstatus)}>{pstatus ? 'Hide' : 'Show'}</button>
       </div>
       <br />
-      <div>
+      <div> 
         <button className={btnModule.error}>Error</button>
         <button className={btnModule.warning}>Warning</button>
         <button onClick={() => setStatus(!status)}>{(status ? 'Hide' : 'Show')}</button>
